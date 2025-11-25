251743d0b06d387944a0c08d3411d7aa1d201786
alpha3002025 <alpha300uk@gmail.com>
2025-11-26 07:27:31 +0900

    feat: bruteforce-recursion (2529)

---
+++ icpc/codeplus-bruteforce-recursion/2529-부등호/2529-practice1.py
@@ -0,0 +1,37 @@
+def solve():
+    k = int(input())
+    signs = input().split()
+
+    used = [False] * 10
+    result = []
+
+    max_num = ""
+    min_num = ""
+
+    def is_valid(a,b,sign):
+        if sign == '<':
+            return a < b
+        else:
+            return a > b
+        
+    def backtrack(depth, num_str):
+        nonlocal max_num, min_num
+
+        if depth == k+1:
+            if not min_num:
+                min_num = num_str
+            max_num = num_str
+            return
+        
+        for i in range(10):
+            if used[i]:
+                continue
+
+            if depth == 0 or is_valid(int(num_str[-1]), 1, signs[depth -1]):
+                used[i] = True
+                backtrack(depth+1, num_str + str(i))
+                used[i] = False
+    
+    backtrack(0, "")
+    print(max_num)
+    print(min_num)
