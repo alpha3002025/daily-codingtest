ba41ac4d968bf33828b3fe863bdf9ac0c9d019fd
alpha3002025 <alpha300uk@gmail.com>
2025-11-27 21:56:44 +0900

    feat: dp (2193)

---
+++ icpc/codeplus-dp/2193-이친수/2193-practice2-sol1-simple-dp.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+## i 자리이면셔 j=1로 끝나는 수, j=0 으로 끝나는 수
+dp = [[0] * (2) for i in range(N+1)]
+
+dp[1][0] = 0
+dp[1][1] = 1
+
+for i in range(2, N+1):
+    dp[i][0] = dp[i-1][0] + dp[i-1][1]
+    dp[i][1] = dp[i-1][0]
+
+print(f"{dp[N][0] + dp[N][1]}")
