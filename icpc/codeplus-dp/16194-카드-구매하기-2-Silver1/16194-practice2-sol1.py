6949ee90fc8b414233fb836fdf780136dd2fd160
alpha3002025 <alpha300uk@gmail.com>
2025-11-27 15:15:26 +0900

    feat: dp (16194)

---
+++ icpc/codeplus-dp/16194-카드-구매하기-2-Silver1/16194-practice2-sol1.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+P = [0] + list(map(int, input().split()))
+
+dp = [float('inf')]*(N+1)
+dp[0] = 0
+
+for i in range(1, N+1):
+    for j in range(1, i+1):
+        dp[i] = min(dp[i], dp[i-j] + P[j])
+
+print(f"{dp[N]}")
