fb853a21ad5be5f87bd9321a6ccd8a90ac276d75
alpha3002025 <alpha300uk@gmail.com>
2025-11-29 22:50:33 +0900

    feat: dp (1699)

---
+++ icpc/codeplus-dp/1699-제곱수의-합-Silver2/1699-practice3-sol1-simple-dp.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+dp = [0] * (N+1)
+
+for i in range(N+1):
+    dp[i] = i
+
+    j=0
+    while j*j <= i:
+        dp[i] = min(dp[i], dp[i - j*j]+1)
+        j+=1
+
+print(dp[N])
