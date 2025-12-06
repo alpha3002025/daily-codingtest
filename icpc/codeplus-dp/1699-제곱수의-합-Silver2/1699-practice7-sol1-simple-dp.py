52cd2fd1b615de532a29af72da1f69da2f23d3e3
alpha3002025 <alpha300uk@gmail.com>
2025-12-06 21:47:58 +0900

    feat: dp (1699)

---
+++ icpc/codeplus-dp/1699-제곱수의-합-Silver2/1699-practice7-sol1-simple-dp.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+dp = [0] * (N + 1)
+
+for i in range(1, N + 1):
+    dp[i] = i
+    j=0
+    while j*j <= i:
+        dp[i] = min(dp[i], dp[i-j*j]+1)
+        j+=1
+    
+print(dp[N])
---
+++ icpc/codeplus-dp/1699-제곱수의-합-Silver2/1699-practice7-sol2-topdown-recursion.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+memo = [-1] * (N + 1)
+
+def backtracking(curr):
+    if memo[curr] != -1:
+        return memo[curr]
+
+    total_cnt = curr
+    j=1
+    while j*j <= curr:
+        total_cnt = min(total_cnt, backtracking(curr - j*j) + 1)
+        j+=1
+    
+    memo[curr] = total_cnt
+    return memo[curr]
+
+answer = backtracking(N)
+print(answer)
