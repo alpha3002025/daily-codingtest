93c1c99a9e53108c43d6c3696dd547f48f94664a
alpha3002025 <alpha300uk@gmail.com>
2025-12-06 21:04:04 +0900

    feat: dp (1912)

---
+++ icpc/codeplus-dp/1912-연속합-Silver2/1912-practice7-sol1-simple-dp.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+
+dp = [0]*N
+dp[0] = A[0]
+
+for i in range(1, N):
+    dp[i] = max(A[i], dp[i-1]+A[i])
+
+print(max(dp))
