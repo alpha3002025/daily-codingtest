61131beba6bd75fa0f7c3eef33e2536c7e4db976
alpha3002025 <alpha300uk@gmail.com>
2025-12-07 23:55:32 +0900

    feat: dp (11722)

---
+++ icpc/codeplus-dp/11722-가장-긴-감소하는-수열-Silver2/11722-practice1-sol1.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+
+dp = [0] * (N)
+
+for i in range(N):
+    dp[i] = 1
+    for j in range(i):
+        if A[i] < A[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(max(dp))
