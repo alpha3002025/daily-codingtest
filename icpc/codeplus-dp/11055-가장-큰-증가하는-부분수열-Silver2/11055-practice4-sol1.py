d8d559facc4353735a3a6f5bd56cf4371796a4cf
alpha3002025 <alpha300uk@gmail.com>
2025-12-07 22:56:21 +0900

    feat: dp (11055)

---
+++ icpc/codeplus-dp/11055-가장-큰-증가하는-부분수열-Silver2/11055-practice4-sol1.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+
+dp = [0] * N
+for i in range(N):
+    dp[i] = A[i]
+    for j in range(i):
+        if A[j] < A[i]:
+            dp[i] = max(dp[i], dp[j] + A[i])
+
+print(max(dp))
