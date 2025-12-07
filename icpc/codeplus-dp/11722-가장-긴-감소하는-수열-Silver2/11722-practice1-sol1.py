9bee8c7e3d7d75922838326e8b89bac34ad6afd7
alpha3002025 <alpha300uk@gmail.com>
2025-12-08 08:08:14 +0900

    feat: dp (11722)

--- icpc/codeplus-dp/11722-가장-긴-감소하는-수열-Silver2/11722-practice1-sol1.py
+++ icpc/codeplus-dp/11722-가장-긴-감소하는-수열-Silver2/11722-practice1-sol1.py
@@ -9,7 +9,7 @@ dp = [0] * (N)
 for i in range(N):
     dp[i] = 1
     for j in range(i):
-        if A[i] < A[j]:
+        if A[j] > A[i]:
             dp[i] = max(dp[i], dp[j] + 1)
 
 print(max(dp))
