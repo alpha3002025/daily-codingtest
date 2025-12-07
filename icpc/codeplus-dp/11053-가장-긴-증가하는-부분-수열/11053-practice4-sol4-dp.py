03b69f33f0f804f165693bcd6b7e93d037572f6b
alpha3002025 <alpha300uk@gmail.com>
2025-12-08 08:38:02 +0900

    feat: dp (11053)

---
+++ icpc/codeplus-dp/11053-가장-긴-증가하는-부분-수열/11053-practice4-sol4-dp.py
@@ -0,0 +1,20 @@
+"""
+11053 : 증가수열에서 오름차순 연속 부분수열 중 가장 큰 '길이'
+11055 : 증가수열에서 오름차순 연속 부분수열 중 가장 큰 '합'
+11722 : 감소수열에서 내림차순 연속 부분수열 중 가장 큰 '길이'
+"""
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+
+dp = [1] * N
+
+for i in range(N):
+    for j in range(i):
+        if A[j] < A[i]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(max(dp))
