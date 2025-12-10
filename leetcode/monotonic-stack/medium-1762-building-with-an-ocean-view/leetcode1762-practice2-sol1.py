10d9b167fcc8937ab803a4450d6ccbf44299a446
alpha3002025 <alpha300uk@gmail.com>
2025-12-10 13:24:26 +0900

    feat: monotonic stack (leetcode 1762. Buildings With an Ocean View)

---
+++ leetcode/monotonic-stack/medium-1762-building-with-an-ocean-view/leetcode1762-practice2-sol1.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+def solution(heights):
+    stack = []
+    answer = []
+    for i in reversed(range(len(heights))):
+        while stack and heights[stack[-1]] < heights[i]:
+            stack.pop()
+        
+        if not stack:
+            answer.append(i)
+
+        stack.append(i)
+    
+    return list(reversed(answer))
+
+heights1 = [4,2,3,1]
+heights2 = [4,3,2,1]
+
+print(solution(heights1))
+print(solution(heights2))
