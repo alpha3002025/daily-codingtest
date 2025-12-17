56493df343df98ec61bb0d071868fe6d48005fe2
alpha3002025 <alpha300uk@gmail.com>
2025-12-17 16:55:44 +0900

    feat: simulation (programmers lv2. 다리를 지나는 트럭)

---
+++ programmers/simulation/lv2-다리를-지나는-트럭/solution-20251217-1.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+def solution(bridge_length, weight, truck_weights):
+    time = 0
+    bridge = deque([0]*bridge_length)
+    trucks = deque(truck_weights)
+    
+    current_weight = 0
+    
+    while bridge:
+        time += 1
+        
+        exited = bridge.popleft()
+        current_weight -= exited
+        
+        if trucks:
+            if current_weight + trucks[0] <= weight:
+                new_truck = trucks.popleft()
+                bridge.append(new_truck)
+                current_weight += new_truck
+            else:
+                bridge.append(0)
+    
+    return time
