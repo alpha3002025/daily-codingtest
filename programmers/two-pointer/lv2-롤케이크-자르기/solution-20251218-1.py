ca9722c7b80fdfcf5a4e50ca9a3b908d85e98ac7
alpha3002025 <alpha300uk@gmail.com>
2025-12-18 09:50:32 +0900

    feat: two pointer, hash (programmers lv2. 롤케이크 자르기)

---
+++ programmers/two-pointer/lv2-롤케이크-자르기/solution-20251218-1.py
@@ -0,0 +1,23 @@
+from collections import Counter
+
+def solution(topping):
+    left = set()
+    right = Counter(topping)
+    
+    len_right = len(right)
+    kind = 0
+    
+    for curr_topping in topping:
+        left.add(curr_topping)
+        right[curr_topping] -= 1
+        
+        ## 만약 현재 curr_topping 이 오른쪽 집합에서 완전히 제거되었다면
+        if right[curr_topping] == 0:
+            ## right 의 종류수 -= 1
+            len_right -= 1
+            
+        if len(left) == len_right:
+            # print(f"left = {left}, right = {right}")
+            kind += 1
+    
+    return kind
