94ff17816c4ecf87d009d124bedac02e6c99b674
alpha3002025 <alpha300uk@gmail.com>
2025-12-18 13:59:28 +0900

    feat: combination,permutation,product (lv2. 모음사전)

--- programmers/combination_permutation_product/lv2-모음사전/solution-20251218-1.py
+++ programmers/combination_permutation_product/lv2-모음사전/solution-20251218-1.py
@@ -3,12 +3,12 @@ from itertools import product
 def solution(word):
     """
     5**1 + 5**2 + 5**3 + 5**4 + 5**5
-    5 + 25 + 125 + 625 + 3125 = 3905 
+    5 + 25 + 125 + 625 + 3125 = 3905
     """
     
     dictionary = []
     for len in range(1, 6):
-        for p in product(['A','E','I','O','U'], repleat=len):
+        for p in product(['A','E','I','O','U'], repeat=len):
             dictionary.append("".join(p))
     
     dictionary.sort()
