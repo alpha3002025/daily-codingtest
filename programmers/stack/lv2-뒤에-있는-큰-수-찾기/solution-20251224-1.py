ef03a148d0ecff27cc4ba3a61cf36725c253f8d3
alpha3002025 <alpha300uk@gmail.com>
2025-12-24 15:04:22 +0900

    feat: monotonic stack (lv2. 뒤에 있는 큰 수 찾기)

---
+++ programmers/stack/lv2-뒤에-있는-큰-수-찾기/solution-20251224-1.py
@@ -0,0 +1,10 @@
+def solution(numbers):
+    stack = []
+    answer = [-1] * len(numbers)
+    
+    for i in range(len(numbers)):
+        while stack and numbers[stack[-1]] < numbers[i]:
+            prev = stack.pop()
+            answer[prev] = numbers[i]
+        stack.append(i)
+    return answer
