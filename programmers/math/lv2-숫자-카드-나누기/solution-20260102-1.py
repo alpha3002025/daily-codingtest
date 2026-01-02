4530744d977a8dffc925cbb71015a2315865bdbb
alpha3002025 <alpha300uk@gmail.com>
2026-01-02 13:55:38 +0900

    feat: math (lv2. 숫자 카드 나누기)

--- programmers/math/lv2-숫자-카드-나누기/solution-20260102-1.py
+++ programmers/math/lv2-숫자-카드-나누기/solution-20260102-1.py
@@ -1,5 +1,28 @@
+from math import gcd
 
+def gcd_from_list(numbers):
+    curr = numbers[0]
+    for i in range(len(numbers)):
+        curr = gcd(curr, numbers[i])
+    return curr
 
+def solution(arrayA, arrayB):
+    gcd_A = gcd_from_list(arrayA)
+    gcd_B = gcd_from_list(arrayB)
+    
+    is_useless_a = False
+    for num in arrayB:
+        if num % gcd_A == 0:
+            is_useless_a = True
+            break
+    
+    is_useless_b = False
+    for num in arrayA:
+        if num % gcd_B == 0:
+            is_useless_b = True
+            break
+    
+    return 0 if is_useless_a and is_useless_b else max(gcd_A, gcd_B)
 
 
 print(solution([10, 17], [5, 20]))
