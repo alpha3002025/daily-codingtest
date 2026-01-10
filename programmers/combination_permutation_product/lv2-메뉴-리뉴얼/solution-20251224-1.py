ef5b13d296d8fb48a8e7c26854db603ea7302f25
alpha3002025 <alpha300uk@gmail.com>
2026-01-10 15:29:50 +0900

    feat: combination, permutation, product (lv2. 모음 사전)

--- programmers/combination_permutation_product/lv2-메뉴-리뉴얼/solution-20251224-1.py
+++ programmers/combination_permutation_product/lv2-메뉴-리뉴얼/solution-20251224-1.py
@@ -23,3 +23,8 @@ def solution(orders, course):
                         answer.append(menu_name)
     
     return sorted(answer)
+
+
+print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2,3,4]))
+print(solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2,3,5]))
+print(solution(["XYZ", "XWY", "WXA"], [2,3,4]))
